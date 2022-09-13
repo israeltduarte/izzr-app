1b0cfab399bef0b831cb439d67e7639064739235
Israel Duarte <israeltduarte@hotmail.com>
2022-09-13 19:19:03 -0300

    update skills to have percent info

--- src/components/SkillArea.js
+++ src/components/SkillArea.js
@@ -6,8 +6,12 @@ const SkillArea = (props) => {
     const [isExpanded, setExpanded] = useState(props.isExpanded);
     const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
     const handleClick = () => setExpanded(!isExpanded);
+
     const area = props.area;
-    const areaResult = ((area.stack.reduce((a, v) => a = a + v.level, 0) / area.stack.length) * 20) + "%";
+    const length = area.stack.length;
+    const percentMultiplier = 100 / length;
+    const areaTotalSum = (area.stack.reduce((a, v) => a = a + v.level, 0));
+    const areaResult = (Math.ceil((areaTotalSum / length) * percentMultiplier)) + "%";
 
     return (
         <div>
@@ -16,21 +20,30 @@ const SkillArea = (props) => {
                     <div className="SkillName">
                         {area.name}
                     </div>
+                    <div className="SkillPercent">
+                        {areaResult}
+                    </div>
                 </div>
             </div>
             <div {...getCollapseProps()}>
                 {
-                    area.stack.map((element) => {
-                        return (
-                            <div className="SkillLine SubLine content" key={element.id}>
-                                <div className="ProgressBar" style={{ width: (element.level * 20) + "%" }} >
-                                    <div className="SkillName">
-                                        {element.name}
+                    area.stack
+                        .sort((a, b) => a.level < b.level ? 1 : -1)
+                        .map((element) => {
+                            let percentElementWidth = element.level * percentMultiplier + "%";
+                            return (
+                                <div className="SkillLine SubLine content" key={element.id}>
+                                    <div className="ProgressBar" style={{ width: percentElementWidth }} >
+                                        <div className="SkillPercent">
+                                            {percentElementWidth}
+                                        </div>
+                                        <div className="SkillName">
+                                            {element.name}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        );
-                    })
+                            );
+                        })
                 }
             </div>
         </div>
